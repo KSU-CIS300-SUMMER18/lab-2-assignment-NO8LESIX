8f71d7493e41bbcd8f846228f5574db677941e8b
Carlos Cerna <cbcerna@ksu.edu>
2018-05-22 14:11:47 -0500

    Added save file and open file as well as the text box to the editor

--- Ksu.Cis300.TextEditor/Form1.Designer.cs
+++ Ksu.Cis300.TextEditor/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.uxFileMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oPenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.uxOpenFileDialogue = new System.Windows.Forms.OpenFileDialog();
+            this.uxSaveFileDialogue = new System.Windows.Forms.SaveFileDialog();
+            this.uxFileMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uxFileMenu
+            // 
+            this.uxFileMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.uxFileMenu.Location = new System.Drawing.Point(0, 0);
+            this.uxFileMenu.Name = "uxFileMenu";
+            this.uxFileMenu.Size = new System.Drawing.Size(484, 24);
+            this.uxFileMenu.TabIndex = 0;
+            this.uxFileMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oPenToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // oPenToolStripMenuItem
+            // 
+            this.oPenToolStripMenuItem.Name = "oPenToolStripMenuItem";
+            this.oPenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.oPenToolStripMenuItem.Text = "Open...";
+            this.oPenToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsToolStripMenuItem_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(12, 27);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(460, 622);
+            this.TextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 661);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.uxFileMenu);
+            this.MainMenuStrip = this.uxFileMenu;
+            this.Name = "Form1";
+            this.Text = "Text Editor";
+            this.uxFileMenu.ResumeLayout(false);
+            this.uxFileMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip uxFileMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oPenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.OpenFileDialog uxOpenFileDialogue;
+        private System.Windows.Forms.SaveFileDialog uxSaveFileDialogue;
     }
 }
 
--- Ksu.Cis300.TextEditor/Form1.cs
+++ Ksu.Cis300.TextEditor/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*Ksu.Cis300.TextEditor.cs
+ * Author: Joshua Weese
+ * Modified by: Carlos Bryant Cerna
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +16,37 @@ namespace Ksu.Cis300.TextEditor
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Main form used to contain all components of the text editor
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Opens the OpenFileDialogue menu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (uxOpenFileDialogue.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Cannot Open File:" + uxOpenFileDialogue.FileName);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (uxSaveFileDialogue.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Cannot Save File:" + uxSaveFileDialogue.FileName);
+            }
         }
     }
 }
